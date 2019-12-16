ba164db9991addf47d4f52b31f3e86184e3a6704
kubersharma001 <kubersharma001@gmail.com>
2019-12-16 19:57:31 +0530

    props: For now lets just log Privileged permissions
        * organize props
    
    Change-Id: I1ff4a7b65db4c988a6e99aefcadf242e450f0c27

--- config/common.mk
+++ config/common.mk
@@ -61,7 +61,7 @@ PRODUCT_COPY_FILES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=log
 
 # Include AOSP audio files
 include vendor/canned/config/aosp_audio.mk
