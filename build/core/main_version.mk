4c7d040b5f2e8386055e035cee21c51e00eac56e
Rajkale99 <kaleraj.rk@gmail.com>
2020-10-31 22:30:41 +0000

    vendor:Update CannedOS System Version props

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,20 +6,13 @@ endif
 
 # LineageOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
+    ro.canned.buildtype=$(CANNED_BUILD_TYPE) \
+    ro.canned.build.date=$(CANNED_BUILD_DATE) \
+    ro.canned.display.version=$(CANNED_DISPLAY_VERSION) \
+    ro.canned.android_version=$(ANDROID_VERSION) \
+    ro.canned.releasetype=$(CANNED_BUILD_TYPE) \
+    ro.cannedversion=$(CANNEDVERSION) \
     ro.canned.version=$(CANNED_VERSION) \
-    ro.canned.releasetype=$(CANNED_BUILDTYPE) \
-    ro.canned.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(CANNED_VERSION) \
-    ro.cannedlegal.url=https://lineageos.org/legal
-
-# LineageOS Platform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.canned.display.version=$(CANNED_DISPLAY_VERSION)
-
-# LineageOS Platform SDK Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.canned.build.version.plat.sdk=$(CANNED_PLATFORM_SDK_VERSION)
-
-# LineageOS Platform Internal Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.canned.build.version.plat.rev=$(CANNED_PLATFORM_REV)
+    ro.modversion=$(CANNED_MOD_VERSION) \
+    ro.canned.ziptype=$(CANNED_VARIANT) \
+    org.canned.fingerprint=$(CANNED_FINGERPRINT)
