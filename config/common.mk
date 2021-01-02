Project-LegionOS/vendor_legion

copy backuptool files to system
For some reason in android11 it fails to copy to $OUT/install, so copy to system as a temporary workaround till the root cause is discovered

config/common.mk
@@ -20,8 +20,8 @@ endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/legion/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/legion/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/legion/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/legion/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/legion/prebuilt/common/bin/50-legion.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-legion.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)

CannedOS/vendor_canned

config/common.mk
@@ -20,8 +20,8 @@ endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/canned/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/canned/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/canned/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/canned/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/canned/prebuilt/common/bin/50-canned.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-canned.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)