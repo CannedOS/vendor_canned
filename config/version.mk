AbdullahTabbal/vendor_legion

Update logic for versioning
This fixes the random naming of zip when doing dirty build

config/version.mk
@@ -16,7 +16,7 @@ ANDROID_VERSION := 11.0
 LEGIONVERSION := v3.4
 
 LEGION_BUILD_TYPE ?= Unofficial
-LEGION_BUILD_DATE := $(shell date +%Y%m%d)
+LEGION_BUILD_DATE := $(shell date +%Y%m%d-%H%M)
 TARGET_PRODUCT_SHORT := $(subst legion_,,$(LEGION_BUILD))
 
 # ZIP TYPE

CannedOS/vendor_canned

config/version.mk
@@ -16,7 +16,7 @@ ANDROID_VERSION := 11.0
 CANNED_VERSION := v1.0
 
 CANNED_BUILD_TYPE ?= UNOFFICIAL
-CANNED_BUILD_DATE := $(shell date +%Y%m%d)
+CANNED_BUILD_DATE := $(shell date +%Y%m%d-%H%M)
 TARGET_PRODUCT_SHORT := $(subst canned_,,$(CANNED_BUILD))
 
 # ZIP TYPE